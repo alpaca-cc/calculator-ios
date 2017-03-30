21a5e68dcf7cca440403bb098877cc3881e518b6
CC <chchen146@gmail.com>
2017-03-30 02:16:08 -0500

    Calculator basic setup

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,19 @@
+//
+//  CalculateBrain.swift
+//  Calculator
+//
+//  Created by CHEN CHEN on 3/30/17.
+//  Copyright © 2017 CHEN CHEN. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorBrain{
+    
+    
+    
+    func performOperation(_ symbol: String) {
+        
+    }
+    
+}
